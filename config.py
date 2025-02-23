d718fa3a1a8ce6f00691b06723a554b3a0c35eb5
balak853 <kingcrp8@gmail.com>
2025-02-23 19:29:28 +0530

    config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@
 API_ID = "---"  # Replace this API ID with your actual API ID
 API_HASH = "----"  # Replace this API HASH with your actual API HASH
 SESSION_STRING = "----"  # Replace this SESSION STRING with your actual SESSION_STRING
-BOT_TOKEN = "Your BOT TOKEN"  # Replace this BOT_TOKEN
+BOT_TOKEN = "7844935766:AAGgOiYyzeKR7xVu-hY_jEvAa6EheXcO4Fk"  # Replace this BOT_TOKEN
 
 # Admin IDs
-ADMIN_IDS = [7275888505, 12345678]
+ADMIN_IDS = [7028548502, 12345678]
 
 # Limits
 DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
